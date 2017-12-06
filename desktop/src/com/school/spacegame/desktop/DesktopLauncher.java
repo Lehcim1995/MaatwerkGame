e3afbde436d0e10a32446d97248727e9723c48a5
michel.jacobs <michel.jacobs@indicia.nl>
2017-12-06 12:42:32 +0100

    wat?

--- desktop/src/com/school/spacegame/desktop/DesktopLauncher.java
+++ desktop/src/com/school/spacegame/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.school.spacegame.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//TODO set settings
+        // fullscreen
+        // Game name
+        // etc
+
 		new LwjglApplication(new Main(), config);
 	}
 }
