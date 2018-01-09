7b69b325f48b88e802393da0d055d9f3cfca88fe
michel.jacobs <michel.jacobs@indicia.nl>
2018-01-09 22:50:16 +0100

    Small start RMI server

---
+++ server/src/classes/ServerMain.java
@@ -0,0 +1,40 @@
+package classes;
+
+import interfaces.IServer;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerMain implements IServer
+{
+    private transient Registry registry;
+
+    public ServerMain() throws RemoteException
+    {
+        registry = LocateRegistry.createRegistry(portNumber);
+        registry.rebind(ServerManger, this);
+//        registry.rebind(connection, connectionInstance); // Database shizzle
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            new ServerMain();
+        }
+        catch (RemoteException e)
+        {
+            Logger.getAnonymousLogger().log(Level.SEVERE, "Could not instantiate a server");
+        }
+    }
+
+    @Override
+    public ArrayList<String> getLobbies() throws RemoteException
+    {
+        return null;
+    }
+}
